d1ba003cc11559837c23b364be1e4b1bfcd9ea1c
FoxxIgor <110695245+FoxxIgor@users.noreply.github.com>
2022-10-31 18:29:27 -0300

    Add files via upload

--- quiz.js
+++ quiz.js
@@ -2,7 +2,8 @@ var j1 = localStorage.getItem("nome1");
 var j2 = localStorage.getItem("nome2");
 var j1Score = 0;
 var j2Score = 0;
-var numero = 0;
+var numeroJ2 = 0;
+var numeroJ1 = 0;
 var conferirr = j2;
 document.getElementById("player1").innerHTML=j1;
 document.getElementById("player2").innerHTML=j2;
@@ -15,7 +16,7 @@ function enviar(){
     /* Tia, o parseInt n ta funcionando, nem na aula esta funcionando */
     input1Value = document.getElementById("n1Input").value;
     input2Value = document.getElementById("n2Input").value;
-    
+    resultadoReal = parseInt(input1Value)*parseInt(input2Value); /* */
 
     value = input1Value+" x "+input2Value;
     console.log(value);
@@ -26,17 +27,16 @@ function enviar(){
     button = "<button id='buttonResposta' onclick='conferir()'> Conferir </button>";
     document.getElementById("apararicao").innerHTML=title+conta+input+button;
 
-    resultadoReal = input1Value*input2Value;
     console.log(resultadoReal);
 
 }
 function conferir(){
     playerResposta = document.getElementById("inputResposta").value;
     if(playerResposta == resultadoReal){
-        numero++; /* Incrementa a var numero */
-        console.log(numero);
+
         if(conferirr == j2){
-            document.getElementById("player2Points").innerHTML=numero; /* Mostra nos pontos do j2 o numero */
+            numeroJ2++; /* Incrementa a var numero */
+            document.getElementById("player2Points").innerHTML=numeroJ2; /* Mostra nos pontos do j2 o numero */
             console.log("acertou!");
             document.getElementById("turnoPergunta").innerHTML="Turno de pergunta do: "+j2; /* Mostra que agora o turno de resposta e do j2 */
             document.getElementById("turnoResposta").innerHTML="Turno de resposta do: "+j1; /* Mostra que agora o turno de resposta e do j1 */
@@ -45,8 +45,9 @@ function conferir(){
             document.getElementById("apararicao").innerHTML=""; /* Reseta a div */
             conferirr = j1;
         }
-        if(conferirr == j1){
-            document.getElementById("player1Points").innerHTML=numero; /* Mostra nos pontos do j2 o numero */
+        else{
+            numeroJ1++; /* Incrementa a var numero */
+            document.getElementById("player1Points").innerHTML=numeroJ1; /* Mostra nos pontos do j2 o numero */
             console.log("acertou!");
             document.getElementById("turnoPergunta").innerHTML="Turno de pergunta do: "+j1; /* Mostra que agora o turno de resposta e do j2 */
             document.getElementById("turnoResposta").innerHTML="Turno de resposta do: "+j2; /* Mostra que agora o turno de resposta e do j1 */
